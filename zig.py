b553573435444edbfcc7cfc1eb5dacc0f23d5efb
Somnath Nandi <somnathnandi368@gmail.com>
2025-04-10 11:36:52 +0530

    update zig.py

--- zig.py
+++ zig.py
@@ -13,15 +13,11 @@ def install_zig(cwd, install_dir):
 
     # clone the git
     subprocess.run(["git", "clone", "https://github.com/ziglang/zig.git"])
-
-
-    os.chdir("./zig")
+    os.chdir("../zig")
     os.mkdir("build")
-    # os.mkdir("bin")
     subprocess.run(["cmake", "-S", ".", "-B", "build", f"-DCMAKE_INSTALL_PREFIX={install_dir}"])
-    # subprocess.run(["mkdir"])
     os.chdir("./build")
-    subprocess.run(["make", "-j8"], shell=True)
+    subprocess.run(["make", "-j8"])
     subprocess.run(["make", "install"])
 
     # resetting the path
@@ -32,9 +28,6 @@ def install_zig(cwd, install_dir):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("zig.py", "zig build script") 
     parser.add_argument("-o", "--outpath", type=str, help="the install path", default="bin")
-    # parser.add_argument("-t", "--type", type=str, help="the type of release mode by default it is RELEASE [DEBUG, RELEASE options]", default="RELEASE")
     args = parser.parse_args()
-    
     cwd = os.getcwd()
-    
     install_zig(cwd, args.outpath)
