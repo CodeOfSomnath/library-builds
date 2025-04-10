25feff53ae79261a89249fb61fd39881b1a096b8
Somnath Nandi <somnathnandi368@gmail.com>
2025-04-10 12:13:03 +0530

    support for ninja

--- llvm.py
+++ llvm.py
@@ -4,7 +4,7 @@ import sys
 import argparse
 
 
-def install_llvm(cwd, install_dir = None, release = "Debug"):
+def install_llvm(cwd, install_dir=None, release="Debug"):
     # install build dependencies
     subprocess.run(["sudo", "apt", "update"])
     subprocess.run(["sudo", "apt", "install", "-y", "cmake", "python3-yaml"])
@@ -12,27 +12,60 @@ def install_llvm(cwd, install_dir = None, release = "Debug"):
     # clone the git project
     subprocess.run(["git", "clone", "https://github.com/llvm/llvm-project.git"])
 
-
     # setup and configure
     os.chdir("./llvm-project")
     if install_dir == None:
-        subprocess.run(["cmake", "-S", "llvm", "-B", "build", f"-DCMAKE_BUILD_TYPE={release.capitalize()}"])
+        subprocess.run(
+            [
+                "cmake",
+                "-S",
+                "llvm",
+                "-B",
+                "build",
+                f"-DCMAKE_BUILD_TYPE={release.capitalize()}",
+            ]
+        )
     else:
-        subprocess.run(["cmake", "-S", "llvm", "-B", "build", f"-DCMAKE_INSTALL_PREFIX={install_dir}", f"-DCMAKE_BUILD_TYPE={release}"])
+        subprocess.run(
+            [
+                "cmake",
+                "-G",
+                "Ninja",
+                "-S",
+                ".",
+                "-B",
+                "build",
+                f"-DCMAKE_INSTALL_PREFIX={install_dir}",
+            ],
+            check=True,
+        )
 
     # build
-    os.chdir("build")
-    subprocess.run(["make", "-j8"])
-    subprocess.run(["make", "install"])
-    
+    # os.chdir("build")
+    # subprocess.run(["make", "-j8"])
+    # subprocess.run(["make", "install"])
+    # Build using Ninja (automatically uses all available cores)
+    subprocess.run(["ninja", "-C", "build"], check=True)
+
+    # Install
+    subprocess.run(["ninja", "-C", "build", "install"], check=True)
+
     # resetting the path
     os.chdir(cwd)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("llvm.py", "llvm build script") 
-    parser.add_argument("-o", "--outpath", type=str, help="the install path", default=None)
-    parser.add_argument("-t", "--type", type=str, help="the type of release mode by default it is RELEASE [DEBUG, RELEASE options]", default="RELEASE")
+    parser = argparse.ArgumentParser("llvm.py", "llvm build script")
+    parser.add_argument(
+        "-o", "--outpath", type=str, help="the install path", default=None
+    )
+    parser.add_argument(
+        "-t",
+        "--type",
+        type=str,
+        help="the type of release mode by default it is RELEASE [DEBUG, RELEASE options]",
+        default="RELEASE",
+    )
     args = parser.parse_args()
     cwd = os.getcwd()
     install_llvm(cwd, args.outpath, args.type)
--- zig.py
+++ zig.py
@@ -11,24 +11,47 @@ def install_zig(cwd, install_dir):
     # installing llvm for the output
     install_llvm(cwd)
 
-    # clone the git
+    # clone the git and build
     subprocess.run(["git", "clone", "https://github.com/ziglang/zig.git"])
     subprocess.run(["ls", "-al"])
     os.chdir("./zig")
-    os.mkdir("build")
-    subprocess.run(["cmake", "-S", ".", "-B", "build", f"-DCMAKE_INSTALL_PREFIX={install_dir}"])
-    os.chdir("./build")
-    subprocess.run(["make", "-j8"])
-    subprocess.run(["make", "install"])
+    # os.mkdir("build")
+    # # subprocess.run(
+    # #     ["cmake", "-S", ".", "-B", "build", f"-DCMAKE_INSTALL_PREFIX={install_dir}"]
+    # # )
+    # os.chdir("./build")
+    # subprocess.run(["make", "-j8"])
+    # subprocess.run(["make", "install"])
+
+    subprocess.run(
+        [
+            "cmake",
+            "-G",
+            "Ninja",  # Use Ninja generator
+            "-S",
+            ".",
+            "-B",
+            "build",
+            f"-DCMAKE_INSTALL_PREFIX={install_dir}",
+        ],
+        check=True
+    )
+
+    # Build using Ninja (automatically uses all available cores)
+    subprocess.run(["ninja", "-C", "build"], check=True)
+
+    # Install
+    subprocess.run(["ninja", "-C", "build", "install"], check=True)
 
     # resetting the path
     os.chdir(cwd)
 
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("zig.py", "zig build script") 
-    parser.add_argument("-o", "--outpath", type=str, help="the install path", default="bin")
+    parser = argparse.ArgumentParser("zig.py", "zig build script")
+    parser.add_argument(
+        "-o", "--outpath", type=str, help="the install path", default="bin"
+    )
     args = parser.parse_args()
     cwd = os.getcwd()
     install_zig(cwd, args.outpath)
